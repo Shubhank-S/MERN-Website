679c2695c7f28e5909cb29f4855dffafdc21c125
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-11-27 20:09:29 +0530

    Routing setup done in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>This is Home page</h1>} />
+        <Route path="/about" element={<h1>This is About page</h1>} />
+        <Route path="/register" element={<h1>This is Register page</h1>} />
+        <Route path="/login" element={<h1>This is Login page</h1>} />
+        <Route path="*" element={<h1>This is Error page</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
