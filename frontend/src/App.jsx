8f07eb0da9c33543250f712fed2bcd9698bb3539
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-11-27 22:11:33 +0530

    Created Pages for the Website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Service from "./pages/Service/Service";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1>This is Home page</h1>} />
-        <Route path="/about" element={<h1>This is About page</h1>} />
-        <Route path="/service" element={<h1>This is Service page</h1>} />
-        <Route path="/register" element={<h1>This is Register page</h1>} />
-        <Route path="/login" element={<h1>This is Login page</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<h1>This is Error page</h1>} />
       </Routes>
     </BrowserRouter>
